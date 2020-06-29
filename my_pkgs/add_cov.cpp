16a6ba5f7babc0f5783afc4b0c1f39b96a73047f
woodjosh <woodjosh@umich.edu>
2020-06-29 16:28:52 -0400

    try 2

---
+++ my_pkgs/add_cov.cpp
@@ -0,0 +1,50 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
+class SubscribeAndPublish
+{
+public:
+  SubscribeAndPublish()
+  {
+    //Topic you want to publish
+    pub_ = n_.advertise<geometry_msgs::PoseWithCovarianceStamped>("/orb_slam2_mono/pose_cov", 1);
+
+    //Topic you want to subscribe
+    sub_ = n_.subscribe("/orb_slam2_mono/pose", 1, &SubscribeAndPublish::callback, this);
+  }
+
+  void callback(const geometry_msgs::PoseStamped& input)
+  {
+    geometry_msgs::PoseWithCovarianceStamped output;
+    output.header = input.header;
+    output.pose.pose = input.pose;
+    output.pose.covariance[0] = 0.1; 
+    output.pose.covariance[7] = 0.1; 
+    output.pose.covariance[14] = 0.1;
+    output.pose.covariance[21] = 0.1;
+    output.pose.covariance[28] = 0.1;
+    output.pose.covariance[35] = 0.1;
+    pub_.publish(output);
+  }
+
+private:
+  ros::NodeHandle n_; 
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+
+};//End of class SubscribeAndPublish
+
+int main(int argc, char **argv)
+{
+  //Initiate ROS
+  ros::init(argc, argv, "add_cov_node");
+
+  //Create an object of class SubscribeAndPublish that will take care of everything
+  SubscribeAndPublish SAPObject;
+
+  ros::spin();
+
+  return 0;
+}
